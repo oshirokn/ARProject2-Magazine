b0b135b2979cd9d675802bbb64ba882e6c276bcb
AzzakFeed <oshiro.kn@gmail.com>
2020-05-08 12:16:08 +0300

    Initial Commit - Ignored large images

---
+++ Assets/2- Scripts/ImageRecognitionExample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+
+public class ImageRecognitionExample : MonoBehaviour
+{
+
+
+    [SerializeField] ARTrackedImageManager _arTrackerImageManager;
+
+    [SerializeField] TMPro.TMP_Text _debugText;
+
+
+    public void OnEnable()
+    {
+        _arTrackerImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _arTrackerImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            _debugText.text = "TrackedImage: " + trackedImage.name;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/2- Scripts/SessionState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+public class SessionState : MonoBehaviour
+{
+    // This script gets the session state and displays it in the StateText object
+
+    [SerializeField] TMPro.TMP_Text StateText;
+
+
+    private void OnEnable()
+    {
+
+        ARSession.stateChanged += OnStateChanged;
+    }
+
+    void OnStateChanged(ARSessionStateChangedEventArgs eventArgs)
+    {
+
+        StateText.text = eventArgs.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
